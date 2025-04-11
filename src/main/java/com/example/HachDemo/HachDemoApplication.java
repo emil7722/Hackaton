9be3ffa43ebbc65be186f2bc02a8bfbf672097a5
Emil <emiltahirli0@gmail.com>
2025-04-11 14:00:42 +0100

    Initial commit

---
+++ src/main/java/com/example/HachDemo/HachDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HachDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HachDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HachDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HachDemo/HachDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HachDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HachDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
